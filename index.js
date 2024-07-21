b27a2de278604328f3ac49a5d947e6953fb91a89
Rin <150316145+NanoSoCute@users.noreply.github.com>
2024-07-22 02:02:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ app.get('/', (req, res) => {
     res.json(schedule)
 })
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    if (req.method === 'OPTIONS') {
+        res.status(200).end()
+        return
+      }
+    next();
+});
+
 function getCurrentDayAndTime() {
     const now = moment().tz("Asia/Bangkok");
     const day = now.format("dddd");
